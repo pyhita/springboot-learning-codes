ab2b7114616d19b4b694ea10111dcfb83947ca7d
pyhita <pyhita.dev@gmail.com>
2024-03-05 20:08:34 +0800

    feat: bean lifecycle

---
+++ springboot-source-test/spring-ioc-test/src/main/java/com/pyhita/a05/mapper/Mapper1.java
@@ -0,0 +1,7 @@
+package com.pyhita.a05.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Mapper1 {
+}
---
+++ springboot-source-test/spring-ioc-test/src/main/java/com/pyhita/a05/processor/AtBeanPostProcessor.java
@@ -0,0 +1,43 @@
+package com.pyhita.a05.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.type.MethodMetadata;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+
+import java.util.Set;
+
+public class AtBeanPostProcessor implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        try {
+            CachingMetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory();
+            MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(new ClassPathResource("com/pyhita/a05/Config.class"));
+            // 找到所有被@Bean 标注的方法
+            Set<MethodMetadata> methods = metadataReader.getAnnotationMetadata().getAnnotatedMethods(Bean.class.getName());
+            for (MethodMetadata method : methods) {
+                System.out.println(method.getMethodName());
+                // 生成BeanDefinition
+                BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition();
+                // 设置工厂方法
+                builder.setFactoryMethodOnBean(method.getMethodName(), "config");
+                // 设置自动装配模式
+                builder.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_CONSTRUCTOR);
+                // 设置init 方法
+                String m = method.getAnnotationAttributes(Bean.class.getName()).get("initMethod").toString();
+                if (m.length() > 0) {
+                    builder.setInitMethodName(m);
+                }
+                registry.registerBeanDefinition(method.getMethodName(), builder.getBeanDefinition());
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
